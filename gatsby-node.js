a0362eccb08bf19a0fdf3ccb31df3fcb831a9c93
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-10-26 13:43:00 +0100

    remove getconfig

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,12 +14,4 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
       modules: [path.resolve(__dirname, 'src'), 'node_modules']
     }
   })
-
-  const config = getConfig()
-  if (stage.startsWith('develop') && config.resolve) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react-dom': '@hot-loader/react-dom'
-    }
-  }
 }
