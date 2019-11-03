7be92706a1f1a96a8b07b0cdab9705fa4610c57d
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-11-03 13:29:46 +0100

    last BOOOOM ARTISTS Ready to go LIVEEEgit add .git add .

--- src/components/Releases/Releases.js
+++ src/components/Releases/Releases.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { SReleases, SRelease } from './SReleases'
+import { Flex } from 'components'
 
 const Releases = ({ releases = [] }) => {
-  const clonedReleases = releases.map(({ title }, index) => {
+  const clonedReleases = releases.map(({ title, country, barcode }, index) => {
     if (index < 10) {
       return (
         <SRelease>
-          <h1>{title}</h1>
+          <Flex direction="column" x="center" y="center">
+            {title && <h1>{title}</h1>}
+            {country && <h2>{country}</h2>}
+            {barcode && <h2>{barcode}</h2>}
+          </Flex>
         </SRelease>
       )
     } else {
--- src/components/Releases/SReleases.js
+++ src/components/Releases/SReleases.js
@@ -5,12 +5,12 @@ export const SReleases = styled(SResultsList)`
 `
 
 export const SRelease = styled.div`
-  width: 100%;
-  background-color: rgba(255, 255, 255, 0.1);
-  transition: all 0.32s ease-in-out;
+  width: 100% !important;
+  background-color: rgba(0, 0, 0, 0.1) !important;
+  transition: all 0.32s ease-in-out !important;
   &:hover {
-    background-color: rgba(255, 255, 255, 0.18);
-    transform: scale(0.975);
+    background-color: rgba(0, 0, 0, 0.94) !important;
+    transform: scale(0.975) !important;
   }
   @media (min-width: 768px) {
     &:nth-child(odd) {
@@ -19,6 +19,25 @@ export const SRelease = styled.div`
 
     &:nth-child(even) {
       margin-top: 15% !important;
+    }
+  }
+  > div {
+    height: 100%;
+  }
+  > div > * {
+    width: 100% !important;
+    display: flex !important;
+    justify-content: center !important;
+    align-items: center !important;
+    &:first-child {
+      background-color: rgba(167, 202, 253, 0.2) !important;
+    }
+    &:nth-child(2) {
+      background-color: rgba(167, 202, 253, 0.19) !important;
+    }
+
+    &:nth-child(3) {
+      background-color: rgba(167, 202, 253, 0.18) !important;
     }
   }
 `
